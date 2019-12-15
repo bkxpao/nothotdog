29bf0c5269b5a8f618c4d241e82f7bf60c483301
Godthumb <631448504@qq.com>
2019-12-15 23:59:35 +0800

    “修改测试程序”

--- arithmetic/hotdog-classier/valid.py
+++ arithmetic/hotdog-classier/valid.py
@@ -3,59 +3,46 @@ from model import *
 from myDataset import *
 import torchvision.transforms as transforms
 import numpy as np
+import argparse
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+parser = argparse.ArgumentParser()
+parser.add_argument('--test_path', type=str, default='./data/test', help='test path in your computer')
+opt = parser.parse_args()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# 测试代码
-net.to(device)
-net.load_state_dict(torch.load('./checkpoint/NotHotdog.pth'))
+
+# net.to(device)
+net.load_state_dict(torch.load('/home/bkxpao/classifier/checkpoint/NotHotdog.pth'))
 net.eval()
 
 test_transform = transforms.Compose([transforms.Resize((224, 224)),
                                      transforms.ToTensor()])
-myTestDataset = myDataset('./data/test', transform=test_transform)
+
+myTestDataset = myDataset(opt.test_path, transform=test_transform)
 myTestDataloader = DataLoader(myTestDataset, batch_size=1, shuffle=False)
-
-
-
 
 total = 0
 correct = 0
-true_labels = []
-pred_labels = []
-print('waiting...')
+# true_labels = []
+# pred_labels = []
+# print('waiting...')
 with torch.no_grad():
     for idx, data in enumerate(myTestDataloader):
         image, label = data
-        true_labels.append(label.item())
-        cuda_image = image.to(device)
-        cuda_label = label.to(device)
-        outputs = net(cuda_image)
+        # true_labels.append(label.item())
+        # cuda_image = image.to(device)
+        # cuda_label = label.to(device)
+        outputs = net(image)
         _, predicted = torch.max(outputs, 1)
-        pred_labels.append(predicted.cpu().item())
-        total += cuda_label.size(0)
-        correct += (predicted == cuda_label).sum().item()
+        # pred_labels.append(predicted.cpu().item())
+        total += label.size(0)
+        correct += (predicted == label).sum().item()
 
-print(true_labels)
-print(pred_labels)
+# print(true_labels)
+        print(int(predicted.numpy()))
 
 
-print('Accuracy of the network on the total test images: %d %%' % (100 * correct / total))
-recall, precision, f1_score = confusion_mat(np.array(true_labels), np.array(pred_labels))
-print(recall, precision, f1_score)
-print(np.array(myTestDataset.imgs)[np.array(true_labels) != np.array(pred_labels)])
+# print('Accuracy of the network on the total test images: %d %%' % (100 * correct / total))
+# recall, precision, f1_score = confusion_mat(np.array(true_labels), np.array(pred_labels))
+# print(recall, precision, f1_score)
+# print(np.array(myTestDataset.imgs)[np.array(true_labels) != np.array(pred_labels)])
